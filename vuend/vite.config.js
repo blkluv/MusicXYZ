2a17c890163de32751440258b80005f390d76382
WEB5D <info@blkluv.org>
2026-07-09 22:29:19 -0400

    Update vite.config.js

--- vuend/vite.config.js
+++ vuend/vite.config.js
@@ -2,21 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // vueDevTools(),
   ],
+
   server: {
-    host: '0.0.0.0', // 允许外部访问
-    port: 8888,       // 设置端口为 8888
+    host: '0.0.0.0',
+    port: 8888,
   },
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+
+  build: {
+    sourcemap: false,
+    target: 'esnext',
   },
 })
